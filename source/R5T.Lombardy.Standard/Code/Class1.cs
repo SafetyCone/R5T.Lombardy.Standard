5ed3646de6b4e0fc6e93541b78a4dc3c59f5457b
MinexAutomation <minex.automation@gmail.com>
2020-01-19 18:52:27 -0800

    Added service collection extensions.

--- source/R5T.Lombardy.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Lombardy.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Lombardy.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Lombardy.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddStringlyTypedPathOperator(this IServiceCollection services)
+        {
+            services.AddSingleton<IStringlyTypedPathOperator, StringlyTypedPathOperator>();
+
+            return services;
+        }
+    }
+}
