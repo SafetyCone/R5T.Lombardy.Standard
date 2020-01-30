0d9bda1e1c3d758b8b0600de6bd4881473256b6b
MinexAutomation <minex.automation@gmail.com>
2020-01-30 01:48:36 -0800

    Updated IServiceCollection extensions for ServiceAction<T> functionality.

--- source/R5T.Lombardy.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Lombardy.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,16 +2,30 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
+
 
 namespace R5T.Lombardy.Standard
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="IStringlyTypedPathOperator"/> service.
+        /// </summary>
         public static IServiceCollection AddStringlyTypedPathOperator(this IServiceCollection services)
         {
-            services.AddSingleton<IStringlyTypedPathOperator, StringlyTypedPathOperator>();
+            services.AddDefaultStringlyTypedPathOperator();
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IStringlyTypedPathOperator"/> service.
+        /// </summary>
+        public static ServiceAction<IStringlyTypedPathOperator> AddStringlyTypedPathOperatorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IStringlyTypedPathOperator>(() => services.AddStringlyTypedPathOperator());
+            return serviceAction;
         }
     }
 }
